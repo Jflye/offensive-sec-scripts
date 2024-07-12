a0f4531276e092b4afbee119c269de618736c769
Jimmy Å <51186363+Jflye@users.noreply.github.com>
2024-07-12 13:28:30 +0200

    Update autoInfoGathering.sh

--- autoInfoGathering.sh
+++ autoInfoGathering.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
-echo "Enter Host:"
+Green='\033[0;32m'
+NC='\033[0m'
+R='\033[0;31m'
+
+echo -e "${Green}Enter Host:${NC}"
 read host
-echo "$host Selected"
-echo "Enter path for web content fuzzing list... example: /home/user/SecLists/Discovery/Web-Content/raft-medium-directories.txt..."
+echo -e "${Green} $host Selected!${NC}"
+echo -e "${GREEN}Enter path for web content fuzzing list...${NC}"
 read path
-echo "Selected path: "$path"" 
-echo "Scanning host, please wait..."
+echo -e "${Green}Selected path:${NC} "$path""
+
+echo -e "${Green}Scanning host, please wait...${NC}"
 nmap -sV -Pn -A -T 4 "$host"
 whatweb "$host"
 echo "----------------------------------------------------------------------------------------------------------"
-echo "If needed, add the host to /etc/hosts before the next step and run the script again with the host name..."
+echo -e "${R}If needed, add the host to /etc/hosts before the next step and run the script again with the hostname...${NC}"
 echo "----------------------------------------------------------------------------------------------------------"
-echo "Content Discovery In Progress..."
+
+echo -e "${Green}Content Discovery In Progress...${NC}"
 wfuzz -c -z file,"$path" --hc 404,403,301,503 http://"$host"/FUZZ
 echo "----------------------------------------------------------------------------------------------------------"
-echo "Nikto Vulnerability Scan In Progress..."
+
+echo -e "${Green}Nikto Vulnerability Scan In Progress...${NC}"
 nikto +host "$host"
-echo "Finished!"
+echo -e "${Green}Finished!${NC}"
