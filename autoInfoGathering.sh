b4cb84557d40db895047831eefea98051c926de7
Jimmy Å <51186363+Jflye@users.noreply.github.com>
2024-07-12 10:16:33 +0200

    Update autoInfoGathering.sh

--- autoInfoGathering.sh
+++ autoInfoGathering.sh
@@ -9,7 +9,7 @@ echo "Scanning host, please wait..."
 nmap -sV -Pn -A -T 4 "$host"
 whatweb "$host"
 echo "----------------------------------------------------------------------------------------------------------"
-echo "If needed, add the host to /etc/hosts before the next step and run the script again with the DNS entry..."
+echo "If needed, add the host to /etc/hosts before the next step and run the script again with the host name..."
 echo "----------------------------------------------------------------------------------------------------------"
 echo "Content Discovery In Progress..."
 wfuzz -c -z file,"$path" --hc 404,403,301,503 http://"$host"/FUZZ
